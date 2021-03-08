b0dc064428b39e86f7a7f259108b711f558adaef
MaCo <manvacode@chmail.ir>
2021-03-08 18:19:27 +0330

    add some filter for user input data

--- telafo.py
+++ telafo.py
@@ -17,6 +17,7 @@
 #
 from ctypes.util import find_library
 from ctypes import CDLL, c_int, c_char_p, c_double, CFUNCTYPE
+from getpass import getpass
 import json
 import sys
 import time
@@ -70,8 +71,8 @@ _td_set_log_fatal_error_callback.argtypes = [fatal_error_callback_type]
 # initialize TDLib log with desired parameters
 def on_fatal_error_callback(error_message):
     None
-    print('TDLib fatal error: ', error_message)
-    sys.stdout.flush()
+    ###print('TDLib fatal error: ', error_message)
+    ###sys.stdout.flush()
 
 def td_execute(query):
     query = json.dumps(query).encode('utf-8')
@@ -124,6 +125,26 @@ original_base_time = datetime.datetime.now().minute
 
 #################################################
 
+# input value checker
+
+def input_value_checker(input_name, input_value):
+    while (input_value in [None,'\n','',' ']):
+        input_value = input("Please enter Valid Value for " + input_name + ": ")
+    return input_value
+
+
+################################################## 
+
+# hidden input value checker
+
+def hidden_input_value_checker(input_name, input_value):
+    while (input_value in [None,'\n','',' ']):
+        input_value = getpass("Please enter Valid Value for " + input_name + " (hidden typing): ")
+    return input_value
+
+
+#################################################
+
 # function for working with files and read/write configs
 
 def config_read_write(file_name, config_name):
@@ -152,7 +173,9 @@ def config_read_write(file_name, config_name):
                 line_contant = file_read[loop_line].split("=", 1)
                 if (line_contant[0] == config_name):
                     if( (line_contant[1] == '\n' ) or (line_contant[1] == '' ) or (line_contant[1] == None ) ):
+                        input_value = None
                         input_value = (input("Please enter your "+config_name+": "))
+                        input_value = input_value_checker(config_name, input_value)
                         input_value = input_value.split(" ", 1)
                         input_value = input_value[0].strip()
                         if ( (loop_line == 0) ):
@@ -164,14 +187,16 @@ def config_read_write(file_name, config_name):
                         file = open(file_name,"w")
                         file.writelines(file_read)
                         file.close()
-                        print ("Your " + config_name + " Set To: " + input_value + '\n')
+                        print ("Your " + config_name + " set to: " + input_value + '\n--------------------------------')
                         return (input_value.strip())
                     else:
                         line_contant = line_contant[1].split(" ", 1)
                         return (line_contant[0].strip())
 
             if ( ( (line_contant[0] != config_name) ) and (line_counter == file_lines)):
+                input_value = None
                 input_value = (input("Please enter your "+config_name+": "))
+                input_value = input_value_checker(config_name, input_value)
                 input_value = input_value.split(" ", 1)
                 input_value = input_value[0].strip()
                 if( '\n' in file_read[loop_line] ):
@@ -181,7 +206,7 @@ def config_read_write(file_name, config_name):
                 file = open(file_name,"w")
                 file.writelines(file_read)
                 file.close()
-                print ("Your " + config_name + " Set To: " + input_value + '\n')
+                print ("Your " + config_name + " set to: " + input_value + '\n--------------------------------')
                 return (input_value.strip())
 
             line_counter += 1
@@ -290,34 +315,34 @@ def except_exit_func():
 
 #################################################
 
-# save and read configs in "config.txt" file
-
-my_api_id = config_read_write(file_name , "api_id")
-my_api_hash = config_read_write(file_name , "api_hash")
-my_chat_id = config_read_write(file_name , "chat_id")
-#################################################
-
-# select run mode of app
+# select run mode and Use mode of app
 
 try:
     run_mode = None
     use_mode = None
 
-    run_mode = input("\nPlease select run mode:\n'1' -> Auto OFF\n'9' -> Always ON : ")
+    run_mode = input("\nPlease select Run Mode:\n'1' -> Always ON\n'9' -> Auto OFF : ")
     while (run_mode not in ['1','9']):
-        run_mode = input("Please select Valid Value: ")
+        run_mode = input("Please select Valid Value for Run Mode: ")
     
     print ("--------------------------------")
 
-    use_mode = input("Please select use mode:\n'1' -> Sync\n'5' -> Sync & View\n'9' -> Sync & View & Forward : ")
+    use_mode = input("Please select Use Mode:\n'1' -> Sync\n'5' -> Sync & View\n'9' -> Sync & View & Forward: ")
     while (use_mode not in ['1','5','9']):
-        use_mode = input("Please select Valid Value: ")
+        use_mode = input("Please select Valid Value for Use Mode: ")
 
     print ("--------------------------------")
 
 except (KeyboardInterrupt):
     except_exit_func()
 
+#################################################
+
+# save and read configs in "config.txt" file
+
+my_api_id = config_read_write(file_name , "api_id")
+my_api_hash = config_read_write(file_name , "api_hash")
+my_chat_id = config_read_write(file_name , "chat_id")
 #################################################
 
 # main events cycle
@@ -334,9 +359,10 @@ try:
         #call this functoins when app is idle
         if (not event):
             if (run_mode == '1'):
-                idle_counter = not_event_counter(idle_counter)
-            if (run_mode == '9'):
                 not_event_controller=not_event(not_event_controller[0], not_event_controller[1])
+            if (run_mode == '9'):
+                idle_counter = not_event_counter(idle_counter)
+                
 
         # timer for normalising app working speed and decrease process pressure on system
         time.sleep(0.3)
@@ -375,23 +401,48 @@ try:
 
                 # enter phone number to log in
                 if auth_state['@type'] == 'authorizationStateWaitPhoneNumber':
-                    phone_number = input('\nPlease enter your phone number: ')
+                    sys.stdout.write('\r             \r')
+                    sys.stdout.flush()
+                    def phone_number_func():
+                        phone_number = input('Please enter your Phone Number with Country Code: ')
+                        phone_number = input_value_checker("Phone Number", phone_number)
+                        while ('+' not in phone_number):
+                            phone_number = input("Please enter Valid Value for Phone Number: ")
+                        confirm_phone_number = input('Do you confirm "'+ phone_number +'" is your Phone Number? [Y/n] ')
+                        while (confirm_phone_number not in ['Y','N','y','n']):
+                            phone_number = input("Please enter Valid Value for Confirm Phone Number: ")
+                        if ((confirm_phone_number == 'N') or (confirm_phone_number == 'n')):
+                            phone_number_func()
+                        if ((confirm_phone_number == 'Y') or (confirm_phone_number == 'y')):
+                            return phone_number
+                    phone_number = phone_number_func()
                     td_send({'@type': 'setAuthenticationPhoneNumber', 'phone_number': phone_number})
 
                 # wait for authorization code
                 if auth_state['@type'] == 'authorizationStateWaitCode':
-                    code = input('Please enter the authentication code you received: ')
+                    sys.stdout.write('\r             \r')
+                    sys.stdout.flush()
+                    code = getpass('Please enter the Authentication Code you received (hidden typing): ')
+                    code = hidden_input_value_checker("Authentication Code", code)
                     td_send({'@type': 'checkAuthenticationCode', 'code': code})
+
 
                 # wait for first and last name for new users
                 if auth_state['@type'] == 'authorizationStateWaitRegistration':
-                    first_name = input('Please enter your first name: ')
-                    last_name = input('Please enter your last name: ')
+                    sys.stdout.write('\r             \r')
+                    sys.stdout.flush()
+                    first_name = input('Please enter your First Name: ')
+                    first_name = input_value_checker("First Name", first_name)
+                    last_name = input('Please enter your Last Name: ')
+                    last_name = input_value_checker("Last Name", last_name)
                     td_send({'@type': 'registerUser', 'first_name': first_name, 'last_name': last_name})
 
                 # wait for password if present
                 if auth_state['@type'] == 'authorizationStateWaitPassword':
-                    password = input('Please enter your password: ')
+                    sys.stdout.write('\r             \r')
+                    sys.stdout.flush()
+                    password = getpass('Please enter your Password (hidden typing): ')
+                    password = hidden_input_value_checker("Password", password)
                     td_send({'@type': 'checkAuthenticationPassword', 'password': password})
 
             # sync
@@ -435,16 +486,22 @@ try:
                     
                     if ( new_time == 55 ):
                         message_minute = 0
+                        message_hour += 1
                     elif ( new_time == 56 ):
                         message_minute = 1
+                        message_hour += 1
                     elif ( new_time == 57 ):
                         message_minute = 2
+                        message_hour += 1
                     elif ( new_time == 58 ):
                         message_minute = 3
+                        message_hour += 1
                     elif ( new_time == 59 ):
                         message_minute = 4
+                        message_hour += 1                        
                     elif ( new_time == 0 ):
                         message_minute = 5
+                        message_hour += 1
                     else:
                         message_minute = new_time + 5
 
